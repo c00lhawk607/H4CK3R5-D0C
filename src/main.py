8c07649192cddd5230fa4f5a3dd990af88dd0971
Jordan Dixon <jordandixon2004@outlook.com>
2020-09-17 16:16:18 +0000

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,48 @@
+import os, time
+try:
+	from clearing import clear
+except:
+	os.system("pip3 install clearing")
+	from clearing import clear
+
+try:
+	from colorama import *
+	init()
+except:
+	os.system("pip3 install colorama")
+	from colorama import *
+	init()
+while True:
+	start = """H3LL0, W3LC0M3 T0 H4CK3R5 D0C!
+1) Create a new Doc
+2) Load an existing Doc\n"""
+
+	clear()
+
+	i = 0
+
+	while i < len(start):
+		print(Fore.GREEN + start[i],sep="", end="",flush= True)
+		time.sleep(0.1)
+		i += 1
+
+	choice = input(Fore.GREEN + "Choice: ")
+
+	nex = "What is the name of the document?\n"
+
+	i = 0
+	while i < len(nex):
+		print(Fore.GREEN + nex[i], sep="", end="", flush= True)
+		time.sleep(0.1)
+		i += 1
+
+
+	name = input("Name: ")
+	name = name
+
+	clear()
+	print("To go into instert mode press i. To quit without saving, if in insert mode press esc, then press :q and hit enter. To save the doc press esc if in insert mode then press :wq and enter.")
+	Style.RESET_ALL
+	time.sleep(2.5)
+	os.system("vim "+name)
+	clear()
